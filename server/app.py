7ce73a5af5dd3a0752f51679452a7f5f4a756d33
Jussi Timonen <mozzie@loa.fi>
2018-06-04 22:53:07 +0300

    Added CORS

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from server.resources.card import Card
 from server.resources.stock import Stock
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(Card, "/cards")
